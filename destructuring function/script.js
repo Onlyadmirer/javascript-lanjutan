948bb7cec74b208d3f96c46e2479a874310699f0
AkmalHstl <akmalrbc6@gmail.com>
2025-07-13 17:28:20 +0800

    menambahkan materi destructuring function

---
+++ destructuring function/script.js
@@ -0,0 +1,31 @@
+// destructuring nilai
+// function kalkulus(a,b){
+//   return {
+//     tambah: a + b,
+//     kurang: a - b,
+//     kali: a * b,
+//     bagi: a / b,
+//   }
+// }
+
+// const {bagi, kali, tambah, kurang} = kalkulus(2,3);
+// console.log(kurang);
+
+
+// destructuring arguments
+// const mhs1 = {
+//   nama: 'Rizki',
+//   umur: 20,
+//   nilai: {
+//     uas: 70,
+//     uts: 80,
+//     tugas: 98,
+//   }
+// }
+
+
+// function cetakMhs({nama, umur, nilai: {uas, uts, t} }) {
+//   return `Nama: ${nama}, Umur: ${umur}, UAS: ${uas}`
+// }
+
+// console.log(cetakMhs(mhs1))
